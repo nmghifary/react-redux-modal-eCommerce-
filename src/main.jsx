bca0732863857b978845b14acb9ce35f5e66052f
Naufal Ghifary <naufal.m.g.54@gmail.com>
2024-12-22 21:50:10 +0700

    deploy vercel speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import store from './store';
+import store from "./store";
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
+    <SpeedInsights />
   </React.StrictMode>
 );
